7466f50a3f5b44baf66a82f2f90c09322c934a7f
BramVander <bram1105@hotmail.com>
2024-01-13 13:25:19 +0100

    404

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,12 @@
+import PageNav from "../components/PageNav";
+
 function PageNotFound() {
-  return <div>Not fond :(</div>;
+  return (
+    <div>
+      <PageNav />
+      Not fond :(
+    </div>
+  );
 }
 
 export default PageNotFound;
