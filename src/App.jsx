99c7506739e65a38b7317e0cae1021a257eca662
BramVander <bram1105@hotmail.com>
2024-01-16 10:21:47 +0100

    layout + pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import PageNotFound from "./pages/PageNotFound";
-import Home from "./pages/Home";
+// import AppLayout from "./pages/AppLayout.module.css";
+import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import styles from "./Sidebar.module.css";
+import Footer from "./Footer";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
